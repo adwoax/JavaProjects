5e414227480f513aee541f08a51a1f48407c521c
Adwoa Daki <dans4lunca@gmail.com>
2026-02-20 09:33:24 +0000

    Some simple Java projects

--- shopping cart/shoppingCart.java
+++ shopping cart/shoppingCart.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class shoppingCart {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    String item;
+    double price;
+    int quantity;
+    char currency='$';
+    double total;
+
+    System.out.print("What item would you like to buy?");
+    item = scanner.nextLine();
+    System.out.print("What is the price for each?");
+    price = scanner.nextDouble();
+    System.out.print("How many would you like?");
+    quantity = scanner.nextInt();
+
+    total = price * quantity;
+    System.out.print("\n You have bought " + quantity +" " + item + "/s.");
+    System.out.print(" Your total is " + currency + total);
+
+    scanner.close();
+    }
 }
